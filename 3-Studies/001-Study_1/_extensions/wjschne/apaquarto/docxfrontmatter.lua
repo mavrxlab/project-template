4797bdb496f7d3d00d3cd61a53ec559e1739f7e0
Ryan <ryanstraight@arizona.edu>
2024-02-26 08:31:07 -0700

    Switch to apaquarto for study manuscript. Add acronyms extension

--- 3-Studies/001-Study_1/_extensions/wjschne/apaquarto/docxfrontmatter.lua
+++ 3-Studies/001-Study_1/_extensions/wjschne/apaquarto/docxfrontmatter.lua
@@ -80,11 +80,13 @@ local are_affiliations_different = function(authors)
       --Check if affilations are the name for each authors
       for i, a in ipairs(authors) do
         superii = ""
-        for j, aff in ipairs(a.affiliations) do
-          if j > 1 then
-            superii = superii .. ","
+        if a.affiliations then
+          for j, aff in ipairs(a.affiliations) do
+            if j > 1 then
+              superii = superii .. ","
+            end
+            superii = superii .. aff.number
           end
-          superii = superii .. aff.number
         end
 
         if (not hash[superii]) then
@@ -121,11 +123,13 @@ local get_author_paragraph = function(authors, different)
         authordisplay:extend({pandoc.Str(sep .. stringify(a.name.literal))})
         
         superii = ""
-        for j, aff in ipairs(a.affiliations) do
-          if j > 1 then
-            superii = superii .. ","
+        if a.affiliations then
+          for j, aff in ipairs(a.affiliations) do
+            if j > 1 then
+              superii = superii .. ","
+            end
+            superii = superii .. aff.number
           end
-          superii = superii .. aff.number
         end
         
         if different then
@@ -397,8 +401,25 @@ return {
         end
         body:extend({abstractheader})
         local abstract_paragraph = pandoc.Para(pandoc.Str(""))
-        abstract_paragraph.content:extend(meta.apaabstract or meta.abstract)
-        body:extend({abstract_paragraph})
+        
+        if pandoc.utils.type(meta.apaabstract) == "Inlines" then
+          abstract_paragraph.content:extend(meta.apaabstract or meta.abstract)
+          body:extend({abstract_paragraph})
+        end
+        
+        if pandoc.utils.type(meta.apaabstract) == "Blocks" then
+          meta.apaabstract:walk {
+            LineBlock = function(lb)
+              lb:walk {
+                Inlines = function(el)
+                    local lbpara = pandoc.Para(el)
+                    body:extend({lbpara})
+                end
+              }
+            end
+          }
+        end
+
       end
       
       if meta.keywords then
